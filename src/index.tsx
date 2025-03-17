6c271f060913cba77db385f34b465320a7bde264
lucas.sousa <lucas.sousa@codificar.com.br>
2025-03-17 19:06:37 -0300

    test version

--- src/index.tsx
+++ src/index.tsx
@@ -33,27 +33,7 @@ const server = serve({
       })
     },
 
-    "/api/hello": {
-      async GET(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
-      },
-      async PUT(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
-      },
-    },
-
-    "/api/hello/:name": async (req) => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
-      });
-    },
+    '/version': () => Response.json({ version: '0.0.1' })
   },
 
   development: process.env.NODE_ENV !== "production",
