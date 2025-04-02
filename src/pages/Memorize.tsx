cfb1a29af7040982bd3139a05cb1bbd045a7ae3b
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2025-04-02 18:21:41 -0300

    fix app memorize

--- src/pages/Memorize.tsx
+++ src/pages/Memorize.tsx
@@ -2,11 +2,10 @@ import { Layout } from "@/components/Layout";
 
 import { Table } from "@/components/Table";
 import { Form } from "@/components/Form";
+import { MenuInterface } from "@/components/GenerateRoutes";
+import { AppProvider } from "@/providers/AppProvider";
 
 export function Memorize() {
-
-  const uri = 'phrases'
-
   const fields = {
     english: 'English',
     tag: 'Tag',
@@ -23,20 +22,26 @@ export function Memorize() {
     portuguese: 'Portuguese',
   }
 
-  const menus = {
-    '/memorize': 'Memorize',
-  }
+  const memorizeMenus: MenuInterface[] = [
+    {
+      path: '/memorize',
+      label: 'Memorize',
+      component: Memorize
+    }
+  ]
 
   return (
-    <Layout
-      fields={fields}
-      fieldsTable={fieldsTable}
-      fieldsFilter={fieldsFilter}
-      uri={uri}
-      menus={menus}
-    >
-      <Form />
-      <Table />
+    <Layout menus={memorizeMenus} >
+      
+      <AppProvider
+        fields={fields}
+        fieldsTable={fieldsTable}
+        fieldsFilter={fieldsFilter}
+        uri={'phrases'}
+        >
+        <Form />
+        <Table />
+      </AppProvider>
     </Layout>
   )
 }
