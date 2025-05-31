a929b81c431545271ac0cd592d533fb1af1afa5d
lucas.sousa <lucas.sousa@codificar.com.br>
2025-05-31 18:44:45 -0300

    add diet

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { FoodModule } from './food/food.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { DietModule } from './diet/diet.module';
 
 @Module({
-  imports: [FoodModule, PrismaModule],
+  imports: [FoodModule, PrismaModule, DietModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
